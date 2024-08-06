9e58df24628be425ff6a688a23f82d055e602b2f
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-08-06 23:52:37 +0530

    Create EvaluationOfPostfixNotation.java

---
+++ Stack & queues/EvaluationOfPostfixNotation.java
@@ -0,0 +1,45 @@
+class Solution
+{
+    //Function to evaluate a postfix expression.
+    public static int evaluatePostFix(String S)
+    {
+        // Your code here
+        Stack<Integer> stack = new Stack<>();
+        
+        for(char s : S.toCharArray()){
+            int ans = 0;
+            if(s>='0' && s<='9'){
+                int num = s - '0';
+                stack.push(num);
+            }   
+            else{
+                int num1 = stack.peek();
+                stack.pop();
+                int num2 = stack.peek();
+                stack.pop();
+                
+                switch(s){
+                    case '+':
+                        ans = num2+num1;
+                        stack.push(ans);
+                        break;
+                    case '*':
+                        ans = num2*num1;
+                        stack.push(ans);
+                        break;
+                    case '-':
+                        ans = num2-num1;
+                        stack.push(ans);
+                        break;
+                    case '/':
+                        ans = num2/num1;
+                        stack.push(ans);
+                        break;
+                }
+               
+            }
+             
+        }
+       return stack.peek(); 
+    }
+}
