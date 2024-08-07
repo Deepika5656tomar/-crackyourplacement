37a4e7f15edd8df94d8f206fd90b2e9d59364d7e
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-08-07 14:30:10 +0530

    Create GameOfLife.java

---
+++ Array/GameOfLife.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isValidNeighbor(int x,int y,int[][] board){
+        return (x>=0 && x<board.length && y>=0 && y<board[0].length);
+    }
+    public void gameOfLife(int[][] board) {
+        int[] dx={0,0,1,1,1,-1,-1,-1};
+        int[] dy={1,-1,1,-1,0,0,1,-1};
+        for(int row=0;row<board.length;row++){
+            for(int col=0;col<board[0].length;col++){
+                int count_live_neighbors=0;
+                for(int i=0;i<8;i++){
+                    int curr_x=row+dx[i];
+                    int curr_y=col+dy[i];
+                    if(isValidNeighbor(curr_x,curr_y,board)&& Math.abs(board[curr_x][curr_y])==1){
+                        count_live_neighbors++;
+                    }
+                }
+                if(board[row][col]==1 && (count_live_neighbors<2 || count_live_neighbors>3)){
+                    board[row][col]=-1;
+                }
+                if(board[row][col]==0 && count_live_neighbors==3){
+                    board[row][col]=2;
+                }
+            }
+        }
+        for(int row=0;row<board.length;row++){
+            for(int col=0;col<board[0].length;col++){
+                if(board[row][col]>=1){
+                    board[row][col]=1;
+                }
+                else{
+                    board[row][col]=0;
+                }
+            }
+        }
+    }
+}
