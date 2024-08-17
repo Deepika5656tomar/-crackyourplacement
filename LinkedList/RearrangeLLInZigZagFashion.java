e768253a2aaed92efb6379106a12f82adc5f42f3
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-08-17 23:05:58 +0530

    Create RearrangeLLInZigZagFashion.java

---
+++ LinkedList/RearrangeLLInZigZagFashion.java
@@ -0,0 +1,38 @@
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+*/
+
+class Solution {
+    Node zigZag(Node head) {
+        // add code here.
+        Node temp=head;
+        boolean flag=false;
+        while(temp.next!=null){
+            if(flag==false){
+                if(temp.data>temp.next.data){
+                    int t=temp.data;
+                    temp.data=temp.next.data;
+                    temp.next.data=t;
+                }
+                flag=true;
+            }
+            else{
+                if(temp.data<temp.next.data){
+                    int t=temp.data;
+                    temp.data=temp.next.data;
+                    temp.next.data=t;
+                }
+                flag=false;
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+}
